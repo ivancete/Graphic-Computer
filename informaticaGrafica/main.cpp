6218f2ff10c1d0d5331fb64f9584ab71399ea345
Ivanovic <ivanrm@correo.ugr.es>
2017-09-28 14:51:50 +0200

    Initial Commit

---
+++ informaticaGrafica/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  informaticaGrafica
+//
+//  Created by Iván Rodríguez Millán on 28/9/17.
+//  Copyright © 2017 Iván Rodríguez Millán. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
